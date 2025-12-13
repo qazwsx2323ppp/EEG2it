7ee61bc3a73446c37b4d163cdda4f5742dfd93fc
ctp <2220761584@qq.com>
2025-12-13 22:29:54 +0800

    添加文件路径

--- utils/vis_saliency_text.py
+++ utils/vis_saliency_text.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+project_root = Path(__file__).parent.parent 
+sys.path.append(str(project_root))
 import torch
 import matplotlib.pyplot as plt
 import numpy as np

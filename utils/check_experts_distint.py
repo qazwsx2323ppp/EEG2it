e61351a0e9756d39018e85d501946fc03480664e
ctp <2220761584@qq.com>
2026-01-10 18:01:28 +0800

    check expert distinct

--- utils/check_experts_distint.py
+++ utils/check_experts_distint.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-v = np.load("saliency_outputs_expert/saliency_expert_visual.npy")
-s = np.load("saliency_outputs_expert/saliency_expert_semantic.npy")
+v = np.load("../saliency_outputs_expert/saliency_expert_visual.npy")
+s = np.load("../saliency_outputs_expert/saliency_expert_semantic.npy")
 
 print("corr(v,s) =", np.corrcoef(v, s)[0,1])
 print("mean |v-s| =", np.mean(np.abs(v - s)))
